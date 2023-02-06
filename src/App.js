5e7c7747c2d8cf3be18dbcd860ef73502d42f61b
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-02-06 14:00:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const operations = ["=", "+", "-", "÷", "×"];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display" id="display">
+        12
+      </div>
+
+      <div className="number-container">
+        <button className="redish ac big-horizontal">AC</button>
+        {numbers.map((num) => (
+          <button
+            className={`basic ${num === 0 && "big-horizontal"}`}
+            key={num}
+          >
+            {num}
+          </button>
+        ))}
+        <button className="redish">·</button>
+      </div>
+      <div className="operation-container">
+        {operations.map((op, id) => (
+          <button className="green" key={op}>
+            <div className={`op`}>{op}</div>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
