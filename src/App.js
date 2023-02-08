0a4fd1aca8f348f25844dedb4afd2632e2a3f087
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-02-08 02:28:34 +0200

    almost over

--- src/App.js
+++ src/App.js
@@ -1,88 +1,94 @@
 import "./App.css";
 import React from "react";
 const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-const operations = ["=", "+", "-", "/", "*"];
+const operations = [, "+", "-", "/", "*"];
+const ids = {
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  4: "four",
+  5: "five",
+  6: "six",
+  1: "one",
+  2: "two",
+  3: "three",
+  0: "zero",
+  "+": "add",
+  "-": "subtract",
+  "/": "divide",
+  "*": "multiply",
+};
 class App extends React.Component {
   state = {
-    currentNumber: "0",
-    calc: undefined,
+    lastPressed: undefined,
+    calc: "0",
     operation: undefined,
   };
   handleClick = (e) => {
-    const { currentNumber, calc, operation } = this.state;
+    const { calc, lastPressed } = this.state;
     const { innerText } = e.target;
-
-    if (!Number.isNaN(Number(innerText))) {
-      if (currentNumber === "0") {
-        this.setState({
-          currentNumber: innerText,
-        });
-      } else {
-        this.setState({
-          currentNumber: currentNumber + innerText,
-        });
-      }
-      return;
-    }
     switch (innerText) {
       case "AC": {
         this.setState({
-          currentNumber: "0",
-          calc: undefined,
-          operation: undefined,
+          calc: "0",
+        });
+        break;
+      }
+      case "=": {
+        const evaluated = eval(calc);
+        this.setState({
+          calc: evaluated.toString(),
         });
         break;
       }
       case ".": {
-        if (!currentNumber.includes(".")) {
+        const splitted = calc.split(/[+\-\*\/]/);
+        const last = splitted.slice(-1)[0];
+
+        if (!last.includes(".")) {
           this.setState({
-            currentNumber: currentNumber + innerText,
+            calc: calc + ".",
           });
         }
         break;
       }
-      default: {
-        if (!operation) {
+      default:
+        {
+          let ex = undefined;
+          if (operations.includes(innerText)) {
+            if (operations.includes(lastPressed) && innerText !== "-") {
+              ex = calc.slice(0, -3) + ` ${innerText} `;
+            } else {
+              ex = `${calc} ${innerText} `;
+            }
+          } else {
+            ex = calc === "0" ? innerText : calc + innerText;
+          }
+
           this.setState({
-            operation: innerText,
-            calc: currentNumber,
-            currentNumber: "0",
-          });
-        } else if (innerText === "=") {
-          const evaluated = eval(`${calc} ${operation} ${currentNumber}`);
-          this.setState({
-            operation: undefined,
-            calc: evaluated,
-            currentNumber: evaluated,
-          });
-        } else {
-          const evaluated = eval(`${calc} ${operation} ${currentNumber}`);
-          this.setState({
-            operation: innerText,
-            calc: evaluated,
-            currentNumber: evaluated,
+            calc: ex,
           });
         }
-      }
+        this.setState({
+          lastPressed: innerText,
+        });
     }
   };
 
   render() {
-    const { currentNumber, calc, operation } = this.state;
+    const { calc } = this.state;
     return (
       <div className="calculator">
         <p className="pStyle">{JSON.stringify(this.state)}</p>
         <div className="display" id="display">
-          <small>
-            {calc} {operation}
-          </small>
-          {currentNumber}
+          {calc}
         </div>
 
         <div className="number-container">
           <button
             className="redish ac big-horizontal"
             onClick={this.handleClick}
+            id="clear"
           >
             AC
           </button>
@@ -91,24 +97,36 @@ class App extends React.Component {
               className={`basic ${num === 0 && "big-horizontal"}`}
               key={num}
               onClick={this.handleClick}
+              id={ids[num]}
             >
               {num}
             </button>
           ))}
-          <button className="redish positioning" onClick={this.handleClick}>
+          <button
+            className="redish positioning"
+            onClick={this.handleClick}
+            id="decimal"
+          >
             .
           </button>
         </div>
         <div className="operation-container">
           {operations.map((op, id) => (
-            <button className="green" key={op} onClick={this.handleClick}>
+            <button
+              className="green"
+              key={op}
+              onClick={this.handleClick}
+              id={ids[op]}
+            >
               <div className={`op`}>{op}</div>
             </button>
           ))}
+          <button className="green" onClick={this.handleClick} id="equals">
+            =
+          </button>
         </div>
       </div>
     );
   }
 }
-
 export default App;
